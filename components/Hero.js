a4666871b67fcb437d309551f559215f1ccdd749
giaz <gias.pawaz59@gmail.com>
2021-07-14 00:37:29 +0700

    responsive2

--- components/Hero.js
+++ components/Hero.js
@@ -4,7 +4,7 @@ import Form from "./Form";
 
 export default function Hero() {
   return (
-    <div className="md:container mx-auto font-poppins m-24">
+    <div className="md:container mx-auto font-poppins sm:mt-12 md:mt-24">
       <div className="sm:w-10/12 md:w-8/12 mx-auto">
         <p className="text-blue-light">Unhappy with your website?</p>
         <h1 className="lg:text-5xl sm:text-2xl md:text-4xl font-medium lg:leading-normal sm:leading-normal mt-4">
@@ -13,7 +13,7 @@ export default function Hero() {
           and fast web services
         </h1>
       </div>
-      <div className="mt-10 md:w-11/12 md:mx-auto lg:w-12/12 sm:h-[135px] md:h-[10%]">
+      <div className="mt-10 md:w-11/12 md:mx-auto lg:w-12/12 sm:h-[135px] md:h-[10%] object-cover">
         <img
           src="/assets/images/heroImage.jpg"
           alt=""
